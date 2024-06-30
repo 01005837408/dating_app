d73caf8b077d098fb1354d7b528f753319446883
anas <anas3ez@gmail.com>
2024-06-30 20:08:40 +0300

    changeHomeHeaderTilte

--- lib/feature/home/widget/home_title.dart
+++ lib/feature/home/widget/home_title.dart
@@ -1,6 +1,8 @@
 import 'package:dating_app/core/utils/assets.dart';
 import 'package:dating_app/core/utils/const_text.dart';
 import 'package:dating_app/core/utils/styles.dart';
+import 'package:dating_app/feature/profile_screen/profile_screen.dart';
+import 'package:dating_app/feature/settings_profile/settings_Screen_profile.dart';
 import 'package:flutter/material.dart';
 
 class HomeTitle extends StatelessWidget {
@@ -8,27 +10,42 @@ class HomeTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
         children: [
-          Container(
-
-            height: 50,
-            width: 50,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle
-            ),
-
-            child: Image.asset(Assets.profileImage ),
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                        builder: (BuildContext context) =>
+                            const SettingsScreenProfile()));
+              },
+              icon: const Icon(
+                Icons.settings_input_composite_rounded,
+                color: Colors.black,
+                size: 30,
+              )),
+          Text(
+            ConstText.appName,
+            style: AppStyle.font21bold,
           ),
-          Text(ConstText.appName , style:AppStyle.font21bold ,),
-          IconButton(onPressed: (){},
-              icon: Icon(Icons.menu , color: Colors.black,size: 30,)),
-
-
+          GestureDetector(
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) => const ProfileScreen(),
+              ),
+            ),
+            child: Container(
+              height: 50,
+              width: 50,
+              decoration: const BoxDecoration(shape: BoxShape.circle),
+              child: Image.asset(Assets.profileImage),
+            ),
+          ),
         ],
       ),
     );
