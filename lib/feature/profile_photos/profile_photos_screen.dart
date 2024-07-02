dac41da1601c90fc0c6cd838d6eca4c3e9165817
anas <anas3ez@gmail.com>
2024-07-02 12:03:44 +0300

    add grid_view_photos

--- lib/feature/profile_photos/profile_photos_screen.dart
+++ lib/feature/profile_photos/profile_photos_screen.dart
@@ -1,3 +1,8 @@
+import 'package:dating_app/core/spacing/spacing.dart';
+import 'package:dating_app/core/utils/colors.dart';
+import 'package:dating_app/core/widget/custom_appbar.dart';
+import 'package:dating_app/feature/profile_photos/widgets/grid_view_photos.dart';
+import 'package:dating_app/feature/profile_photos/widgets/profile_photos_header_container.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePhotosScreen extends StatelessWidget {
@@ -6,10 +11,35 @@ class ProfilePhotosScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomAppBar(
+              widget: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.arrow_back_ios_new,
+                  color: Colors.white,
+                ),
+              ),
+              backgroundColor: AppColor.kPrimaryColor,
+              text: ' الصور',
+            ),
+            verticalSpacing(15),
+            const ProfilePhotosHeaderContainer(),
+            verticalSpacing(20),
+            const Divider(
+              indent: 50,
+              endIndent: 50,
+              color: AppColor.kPrimaryColor,
+              thickness: 4,
+            ),
+            const GridViewPhotos(),
+          ],
+        ),
       ),
-      body: Container(),
-    );
+    ));
   }
 }
