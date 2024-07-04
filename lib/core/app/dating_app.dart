4b488b3a863e3c0f4c3516c82f95ada8350c9eb9
anas <anas3ez@gmail.com>
2024-07-04 11:31:32 +0300

    set_up_localization

--- lib/core/app/dating_app.dart
+++ lib/core/app/dating_app.dart
@@ -1,8 +1,8 @@
-import 'package:dating_app/feature/home/screens/bottom_navigation.dart';
-import 'package:dating_app/feature/home/screens/home_screen.dart';
 import 'package:dating_app/feature/splash_screen/splash_screen.dart';
+import 'package:dating_app/generated/l10n.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,14 +11,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const ScreenUtilInit(
-      designSize: Size(360, 690),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
       child: SafeArea(
         child: MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
           debugShowCheckedModeBanner: false,
-          home: SplashScreen(),
+          home: const SplashScreen(),
         ),
       ),
     );
